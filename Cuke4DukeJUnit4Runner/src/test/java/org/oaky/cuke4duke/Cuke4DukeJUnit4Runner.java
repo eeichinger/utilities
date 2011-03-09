1b192b42bfe61a3cda6bc0394f36c72bbf3ec911
Erich Eichinger <eeichinger@gmail.com>
2011-03-09 08:16:30 -0500

    improved version

--- Cuke4DukeJUnit4Runner/src/test/java/org/oaky/cuke4duke/Cuke4DukeJUnit4Runner.java
+++ Cuke4DukeJUnit4Runner/src/test/java/org/oaky/cuke4duke/Cuke4DukeJUnit4Runner.java
@@ -1,47 +1,66 @@
 package org.oaky.cuke4duke;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import cuke4duke.ant.CucumberTask;
-import org.apache.tools.ant.BuildEvent;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.BuildListener;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.CommandlineJava;
 import org.apache.tools.ant.types.Path;
+import org.jruby.Main;
 import org.junit.runner.Description;
-import org.junit.runner.Result;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
 import java.io.File;
+import java.io.PrintStream;
 import java.util.Map;
 
 public class Cuke4DukeJUnit4Runner extends Runner {
 
-    public static class MyCucumberTask extends CucumberTask {
+    public static class InProcessCucumberTask extends CucumberTask {
 
-        public MyCucumberTask() {
+        public InProcessCucumberTask() {
             Project project = new Project();
             setProject(project);
             Path path = new Path(project, System.getProperty("java.class.path"));
             getProject().addReference("jruby.classpath", path);
-//            String jrubyHome = System.getenv("JRUBY_HOME");
-//            System.getenv().put("GEM_HOME", jrubyHome);
-//            System.getenv().put("GEM_PATH", jrubyHome);
         }
+
+//        @Override
+//        public void execute() throws BuildException {
+//            try {
+////                super.execute();    //To change body of overridden methods use File | Settings | File Templates.
+//            } catch (Throwable e) {
+//                throw new Error(e);
+//            }
+//        }
 
         @Override
         protected int executeJava(CommandlineJava commandLine) {
-            int res = super.executeJava(commandLine);
+            ByteOutputStream bos = new ByteOutputStream();
+            Main main = new Main(new ByteInputStream(), new PrintStream(bos), new PrintStream(bos));
+            Main.Status status = main.run(commandLine.getJavaCommand().getArguments());
+
+            int res = status.getStatus(); // super.executeJava(commandLine);
             if (res != 0) {
-                throw new Error("" + res);
+                throw new Error("[BEGIN]" + bos.toString()+"[END]");
             }
             return res;
         }
 
         @Override
         protected File getJrubyHome() {
-            return new File(System.getenv("JRUBY_HOME"));
+            String jruby_home = System.getenv("JRUBY_HOME");
+            if (jruby_home == null) {
+                jruby_home = System.getenv("GEM_HOME");
+            }
+            if (jruby_home == null) {
+                throw new BuildException("you must set either JRUBY_HOME or GEM_HOME environment variable");
+            }
+
+            return new File(jruby_home);
         }
     }
 
@@ -63,7 +82,7 @@ public class Cuke4DukeJUnit4Runner extends Runner {
 
         String featureFile = testClass.getSimpleName();
         if (featureFile.endsWith("Feature")) {
-            featureFile = featureFile.substring(0, featureFile.length()-"Feature".length());
+            featureFile = featureFile.substring(0, featureFile.length() - "Feature".length());
         }
         return featureFile + ".feature";
     }
@@ -75,18 +94,19 @@ public class Cuke4DukeJUnit4Runner extends Runner {
 
     @Override
     public void run(RunNotifier runNotifier) {
-        dumpMap(System.getenv());
-        dumpMap(System.getProperties());
-        
-//        runNotifier.fireTestRunStarted(getDescription());
+        System.out.println("Classpath:" + System.getProperty("java.class.path"));
+//        dumpMap(System.getenv());
+//        dumpMap(System.getProperties());
+
         runNotifier.fireTestStarted(getDescription());
-        CucumberTask task = new MyCucumberTask();
+        CucumberTask task = new InProcessCucumberTask();
         task.setFork(false);
 //        task.setArgs("--require /Users/Shared/workspaces/tmp/cuke-mvn_spike/first/target/test-classes /Users/Shared/workspaces/tmp/cuke-mvn_spike/first/features");
         System.out.println(testClass.getClassLoader().getResource(testClass.getName().replace('.', '/') + ".class"));
 
         String featurePath = "features/" + getFeatureFile();
-        task.setArgs("--require target/test-classes " + featurePath);
+//        task.setArgs("--require target/test-classes " + featurePath);
+        task.setArgs("--require target/test-classes --require " + featurePath + " --require features/AnotherFeature.feature");
         try {
             task.execute();
         } catch (Throwable e) {
@@ -97,7 +117,7 @@ public class Cuke4DukeJUnit4Runner extends Runner {
     }
 
     private static void dumpMap(Map map) {
-        for(Object key:map.keySet()) {
+        for (Object key : map.keySet()) {
             System.out.println(key + "=" + map.get(key));
         }
     }

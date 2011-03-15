c02de2e5a8b48e814b4c09d78f66e6c282e6daaa
Erich Eichinger <eeichinger@gmail.com>
2011-03-14 22:27:57 -0400

    moved cuke4duke-junit to new home at https://github.com/eeichinger/cuke4duke-junit

--- Cuke4DukeJUnit4Runner/junit-runner/src/main/java/org/oaky/cuke4duke/Property.java
+++
@@ -1,7 +0,0 @@
-package org.oaky.cuke4duke;
-
-@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD})
-public @interface Property {
-    String value();
-}
